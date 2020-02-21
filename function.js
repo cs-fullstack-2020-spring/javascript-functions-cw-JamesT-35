1124b46db03af8d8668094ac4349171f7ac27332
JamesT-35 <jamesgtatejr@gmail.com>
2020-02-21 14:20:50 -0600

    Functions

---
+++ function.js
@@ -0,0 +1,39 @@
+// // sanity check
+// name = "James"
+// console.log(name)
+
+
+
+// ### Problem 1
+
+// Create a ```printNumbers``` function to print integers from -25 to 20.
+
+
+// function printNumbers() {
+//     for (let i = -25; i <= 20; i++) {
+//         console.log(i)
+//     }
+// }
+// printNumbers();
+
+
+// ### Problem 2
+// Create a function that’s passed a firstName and lastName. Return lastName comma firstName.
+
+function userName() 
+{
+    return
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
